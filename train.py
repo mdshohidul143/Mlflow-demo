2718128f65def229b6e081eaa8d5f8db2af3498f
mdshohidul143 <mdshohidulislam01311@gmail.com>
2024-01-29 08:27:26 +0600

    mlflow

--- train.py
+++ train.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
         print(f"mae is {mae}")
         print(f"r2 is {r2}")
 
-        mlflow.log_param("alpha",alpha) # key value
-        mlflow.log_param("l1_ratio",l1_ratio) # key value
+        mlflow.log_param("alpha",alpha) 
+        mlflow.log_param("l1_ratio",l1_ratio)
         mlflow.log_metric("rmse",rmse)
         mlflow.log_metric("mae",mae)
         mlflow.log_metric("r2",r2)
